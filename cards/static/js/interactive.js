62a431a7b49c4a5a902ada725d1fd7c40d00fbe5
Johanna <jweinsziehr@gmx.de>
2014-10-17 13:26:01 -0700

    initial commit

---
+++ cards/static/js/interactive.js
@@ -0,0 +1,22 @@
+/**
+ * Created by johanna on 10/16/14.
+ */
+$(document).ready(function() {
+
+
+
+
+    $("#check_in").on('click', function() {
+        checkIn();
+    });
+
+//    $.ajax({
+//        url: '/get_mayor',
+//        type: "GET",
+//        success: function(data) {
+//            console.log(data);
+//            $('#mayor').append("<p>Todays mayor is " + data[0].fields.username + "</p>");
+//        }
+    });
+
+    });
---
+++ cards/tests/test_forms.py
@@ -0,0 +1,39 @@
+from django.core import mail
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from ..models import Player
+from ..forms import EmailUserCreationForm
+
+
+
+class FormTestCase(TestCase):
+    def test_clean_username_exception(self):
+        # Create a player so that this username we're testing is already taken
+        Player.objects.create_user(username='test-user')
+
+        # set up the form for testing
+        form = EmailUserCreationForm()
+        form.cleaned_data = {'username': 'test-user'}
+
+        # use a context manager to watch for the validation error being raised
+        with self.assertRaises(ValidationError):
+            form.clean_username()
+
+    def test_clean_username(self):
+        form = EmailUserCreationForm()
+        form.cleaned_data = {'username': 'test2'}
+        self.assertEqual(form.clean_username(), 'test2')
+
+    def test_register_sends_email(self):
+        form = EmailUserCreationForm()
+        form.cleaned_data = {
+            'username': 'test',
+            'email': 'test@test.com',
+            'password1': 'test-pw',
+            'password2': 'test-pw',
+            }
+        form.save()
+        # Check there is an email to send
+        self.assertEqual(len(mail.outbox), 1)
+        # Check the subject is what we expect
+        self.assertEqual(mail.outbox[0].subject, 'Welcome!')
